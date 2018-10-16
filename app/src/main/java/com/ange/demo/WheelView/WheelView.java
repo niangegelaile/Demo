3f634ce597adfee994f0c819662579e17157df5c
niangegelaile <13750523051@139.com>
2018-10-16 08:21:20 +0800

    wheelView加事件处理
    Signed-off-by: niangegelaile <13750523051@139.com>

--- app/src/main/java/com/ange/demo/WheelView/WheelView.java
+++ app/src/main/java/com/ange/demo/WheelView/WheelView.java
@@ -7,9 +7,11 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Scroller;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,6 +22,8 @@ public class WheelView extends View {
     private List<String> datas;
     private Paint textPaint;
     private int lineHeight;
+    private int textSize=18;
+    private Scroller scroller;
     Rect rect;
     public WheelView(Context context) {
         super(context);
@@ -38,14 +42,14 @@ public class WheelView extends View {
         //画笔
         textPaint=new Paint();
         textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(18);
+        textPaint.setTextSize(dptoPx(18));
         textPaint.setAntiAlias(true);
         textPaint.setStyle(Paint.Style.FILL);
         //该方法即为设置基线上那个点到底是left,center,还是right  这里我设置为center
-        textPaint.setTextAlign(Paint.Align.CENTER);
-        lineHeight=40;
+        textPaint.setTextAlign(Paint.Align.LEFT);
+        lineHeight=dptoPx(40);
         rect=new Rect();
-
+        scroller=new Scroller(context);
     }
 
 
@@ -61,18 +65,50 @@ public class WheelView extends View {
         super.onDraw(canvas);
         int textStartPointX=0;//左下角的点
         int textStartPointY=0;
-        for(int i=0;i<1;i++){
+        int scrolly=getScrollY();
+        textStartPointY=-scrolly;
+        for(int i=0;i<datas.size();i++){
             textPaint.getTextBounds(datas.get(i), 0, datas.get(i).length(), rect);
-            textStartPointY=rect.height();//右下角的点
+//            textStartPointY=rect.height();//右下角的点
             Log.e("onDraw","textStartPointX="+textStartPointX+",textStartPointY="+textStartPointY);
-            canvas.drawText(datas.get(i),textStartPointX,textStartPointY,textPaint);
-//            canvas.save();
-//            canvas.translate(0,lineHeight);
+            canvas.drawText(datas.get(i),textStartPointX,rect.height()+textStartPointY,textPaint);
+            canvas.save();
+            canvas.translate(0,lineHeight);
         }
     }
 
+    private float perX,perY;
+    private float dy,dx;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                perX=event.getX();
+                perY=event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float curX=event.getX();
+                float curY=event.getY();
+                dy=curY-perY;
+                dx=curX-perX;
+                scrollBy(0,(int) dy);
+                perX=curX;
+                perY=curY;
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+
+    private int dptoPx(int dp){
+        DisplayMetrics displayMetrics= getContext().getResources().getDisplayMetrics();
+       float density= displayMetrics.density;
+       return (int)(dp*density);
     }
 }
